2f785ad8ac8de535f2323d283b90920a8304280f
Pavel Shatunov <shatuman@ya.ru>
2022-08-01 21:11:45 +0300

    Singleton

---
+++ VK_GB/Controller/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  VK_GB
+//
+//  Created by Павел Шатунов on 01.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+   
+}
---
+++ VK_GB/Service/Session.swift
@@ -0,0 +1,17 @@
+//
+//  Session.swift
+//  VK_GB
+//
+//  Created by Павел Шатунов on 01.08.2022.
+//
+
+import Foundation
+
+class Session {
+    static let session = Session()
+    
+    var token: String?
+    var userId: Int?
+    
+    private init() {}
+}
